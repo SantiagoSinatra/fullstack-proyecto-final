ada4d0e20e4cc475a48fd1958328f5633692c993
Luna <lunakadysz@gmail.com>
2019-04-11 04:57:58 +0300

    commit de rafa

---
+++ faq.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/FAQ.css">
+
+    <title>elibrary | home</title>
+
+</head>
+
+<body>
+
+    <div class="container p-0 s-contenedor-gral">
+        <div class="row">
+            <nav class="col-12">
+                <h1 class="s-titulo">elibrary<span class="s-span-titulo">.com<span></h1>
+                <h3 class="s-slogan">tus utiles con un solo click</h3>
+                <div class="s-contenedor-nav-links s-opacity">
+                <ul class="s-nav-links">
+                    <li><a href="">inicio</a></li>
+                    <li><a href="formulario.php">login/registrarse</a></li>
+                    <li><a href="">categorias</a></li>
+                    <li><a href="">faq</a></li>
+                    <li><a href="">contacto</a></li>
+                </ul>
+                </div>
+                <div class="s-nav-button">
+                    <div class="s-line1"></div>
+                    <div class="s-line2"></div>
+                    <div class="s-line3"></div>
+                </div>
+            </nav>
+            <section class="preguntas row">
+                <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                        ¿Quienes somos?</a>
+                    </h4>
+                    </div>
+                    <div id="collapse1" class="panel-collapse collapse in">
+                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.</div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                        ¿Que hacemos?</a>
+                    </h4>
+                    </div>
+                    <div id="collapse2" class="panel-collapse collapse">
+                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.</div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                        ¿Que formas de pago tienen?</a>
+                    </h4>
+                    </div>
+                    <div id="collapse3" class="panel-collapse collapse">
+                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.</div>
+                    </div>
+                </div>
+                </div>
+                
+            </section>
+            
+            <footer class="col-12 s-footer">
+                <h3 class="s-footer-text">contacto | tel: 1234-5678 | email: unemail@email.com</h3>
+
+            </footer> 
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="javascript/animations.js"></script>
+</body>
+
+</html>
