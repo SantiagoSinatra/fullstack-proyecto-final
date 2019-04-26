e1f0fd8513eceebfcdae55b6726a21563e5ec033
SantiagoSinatra <santiagosinatra@gmail.com>
2019-04-26 00:31:58 -0300

    creacion de nuevo home, todavia en proceso (40%)

---
+++ home.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="css/home.css">
+
+    <title>Elibrary | Tus utiles en un solo lugar</title>
+
+</head>
+
+<body>
+    <!-- contenedor gral -->
+    <div class="container-fluid p-0">
+        <!-- carousel -->
+        <div class="pt-3 mb-3">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/test1.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/test2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/test3.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <!-- fin carousel -->
+
+        <div class="row ml-2 mr-2 pb-3">
+            <section class="col-12 ss-section-ofertas">
+                <div class="ss-background-ofertas">
+                    <h2 class="ss-titulo-section-ofertas">¡Oferta por tiempo limitado!</h2>
+                    <div class="row mt-3">
+                        <article class="col-6">
+                            <img src="img/ofertas-lapiz-pizzini-1.jpg" class="ss-imagenes-ofertas rounded mx-auto d-block" alt="">
+                            <h3 class="ss-titulo-article-ofertas">Lapiz Pizzini 0.5mm<br>$79.99</h3>
+                        </article>
+                        <article class="col-6">
+                            <img src="img/ofertas-minas-pizzini-2.jpg" class="ss-imagenes-ofertas rounded mx-auto d-block" alt="">
+                            <h3 class="ss-titulo-article-ofertas">Minas Pizzini 0.5mm<br>$49.99</h3>
+                        </article>
+                        <article class="col-6">
+                            <img src="img/ofertas-tablero-plantec-3.jpg" class="ss-imagenes-ofertas rounded mx-auto d-block" alt="">
+                            <h3 class="ss-titulo-article-ofertas">Tablero Plantec<br>$1299.99</h3>
+                        </article>
+                        <article class="col-6">
+                            <img src="img/ofertas-regla-plantec-4.jpg" class="ss-imagenes-ofertas rounded mx-auto d-block" alt="">
+                            <h3 class="ss-titulo-article-ofertas">Regla Plantec Aluminio<br>$299.99</h3>
+                        </article>
+                    </div>
+                    <h2 class="ss-titulo-section-ofertas">Precio Total: $1729.99!</h2>
+                </div>
+            </section>
+            <section class="col-12 ss-section-categorias mt-4">
+                <h2 class="ss-titulo-categorias">Nuestros Productos</h2>
+                <div class="row">
+                    <article class="col-12 ss-categoria-escolares ss-categoria-global-properties">
+                        <h3 class="ss-articulos-categorias ">Escolares</h3>
+                    </article>
+                    <article class="col-12 ss-categoria-dibujo ss-categoria-global-properties">
+                        <h3 class="ss-articulos-categorias ">Dibujo Tecnico</h3>
+                    </article>
+                    <article class="col-12 ss-categoria-oficina ss-categoria-global-properties">
+                        <h3 class="ss-articulos-categorias ">Articulos de Oficina</h3>
+                    </article>
+                    <article class="col-12 ss-categoria-mayorista ss-categoria-global-properties">
+                        <h3 class="ss-articulos-categorias ">Ventas por Mayor</h3>
+                    </article>
+                    <article class="col-12 ss-categoria-inclusivos ss-categoria-global-properties">
+                        <h3 class="ss-articulos-categorias ">Productos Inclusivos</h3>
+                    </article>
+                </div>
+
+            </section>
+        </div>
+
+    </div> <!-- fin contenedor gral -->
+
+
+</body>
+
+<!-- scripts que usa bootstrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- ===squb=== -->
+
+</html>
